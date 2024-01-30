b171f830dac03ddc2a8a56ef2235928c90db605c
leandrofabianjr <leandrofabianjr@gmail.com>
2024-01-30 20:34:03 -0300

    Correção de estado ao fazer logout

--- lib/app/router/guards/auth_guard.dart
+++ lib/app/router/guards/auth_guard.dart
@@ -1,17 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:todavenda/auth/bloc/auth_bloc.dart';
+import 'package:todavenda/cart/bloc/cart_bloc.dart';
 
-Future<String?> authGuard(BuildContext context) async {
+Future<String?> authGuard(BuildContext context) {
   final authBloc = BlocProvider.of<AuthBloc>(context);
 
-  var authState = authBloc.state;
+  final authState = authBloc.state;
 
   if (authState is AuthInitial) {
-    authState = await authBloc.stream.first;
+    return authBloc.stream.first
+        .then((value) => checkAuthState(value, context));
   }
 
+  final route = checkAuthState(authState, context);
+
+  return Future.value(route);
+}
+
+String? checkAuthState(AuthState authState, BuildContext context) {
   if (authState is! AuthSuccess) {
+    BlocProvider.of<CartBloc>(context).add(const CartCleaned());
     return '/login';
   }
   return null;
